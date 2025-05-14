145cd025f557c46d7b8515fb287650268c76677d
Shubham Sharma <8696771553shubham@gmail.com>
2025-05-14 14:11:20 +0530

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Watch from "./pages/Watch";
 
 function App() {
-  return <h1>Chl rha hu</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
